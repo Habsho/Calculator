6dea07a80f917bc69aabfb096bde83f7d15a3ec5
habsho <zahedayham@gmail.com>
2020-02-12 12:36:59 +0100

    calc

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 var input = document.getElementById("input");
 
 function pushBtn(obj) {
+  var pushed = obj.innerHTML;
 
-    var pushed = obj.innerHTML;
-
-    if (pushed == "=") {
-        input.innerHTML = eval(input.innerHTML);
-    } else if (pushed == "AC") {
-        input.innerHTML = "0";
+  if (pushed == "=") {
+    input.innerHTML = eval(input.innerHTML);
+  } else if (pushed == "AC") {
+    input.innerHTML = "0";
+  } else {
+    if (input.innerHTML == "0") {
+      input.innerHTML = pushed;
     } else {
-        if (input.innerHTML == "0") {
-            input.innerHTML = pushed;
-        } else {
-            input.innerHTML += pushed;
-        }
+      input.innerHTML += pushed;
     }
+  }
 }
+
+$(document).ready(function() {
+  $(".habsho").click(function() {
+    $("#input").css("color", "green");
+  });
+});
